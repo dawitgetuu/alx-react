bdea47725ee1c07f11b7473289b25bfb378d7135
dawit getuu <archfun3@gmail.com>
2024-07-24 20:20:03 +0000

    task done

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,8 @@
+
+rt const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) =>
+	isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+export const getLatestNotification = () =>
+	'<strong>Urgent Requirement</strong> - complete by EOD';
