0f43de050bef55e9e98c643c864817633ca31c85
dawit getuu <archfun3@gmail.com>
2024-09-02 18:07:08 +0000

    task

---
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/AppContext.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export const defaultUser = {
+  email: '',
+  password: '',
+  isLoggedIn: false
+}
+
+export const logOut = () => {
+  console.log("logged out..")
+}
+
+export const AppContext = React.createContext({
+  user: defaultUser,
+  logOut: logOut
+})
